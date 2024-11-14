d747e075b61590579072bfefc7211184f8587752
Jess Cuellar Silva <jessinma40@gmail.com>
2024-11-14 10:54:22 -0500

    Added OpenAPI contract and server port in the Customer microproject

---
+++ src/main/java/com/bootcamp/example/mscustomerapi/MsCustomerApiApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.example.mscustomerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCustomerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/example/mscustomerapi/MsCustomerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.example.mscustomerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
